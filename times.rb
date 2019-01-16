0051b90db13d42a792fc779d98531b7f138f55ca
Herman <hrivas5726@aol.com>
2019-01-16 02:30:55 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,9 +1,6 @@
 def using_times
- https://learn.co/tracks/web-development-fundamentals/intro-to-ruby/looping/looping-while-and-until-constructors# using_times = 0
-  loop do
-  using_times += 1 
+ 7.times do
   	puts "Wingardium Leviosa"
-  break if using_times >= 7
   end  
 end
 
